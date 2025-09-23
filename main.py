ab9c0636722b3ad57d27b1a0154f6f7dd4c0332a
DgcWevers <danielle@wevers.nz>
2025-09-23 11:16:26 +0200

    verbose

--- main.py
+++ main.py
@@ -2,15 +2,30 @@ import os
 from dotenv import load_dotenv
 from google import genai
 import sys
+from google.genai import types
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 
-prompt = sys.argv[1]
+if len(sys.argv) < 2:
+    print("Error: prompt is not provided")
+    sys.exit(1)
+else:
+    user_prompt = sys.argv[1]
 
-response = client.models.generate_content(model="gemini-2.0-flash-001", contents=prompt)
+messages = [
+    types.Content(role = 'User',
+                  parts = [types.Part(text=user_prompt)])
+]
 
-print(response.text)
-print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+response = client.models.generate_content(model="gemini-2.0-flash-001", 
+                                          contents=messages)
+
+if "--verbose" in sys.argv:
+    print(f"User prompt: {user_prompt}")
+    print(response.text)
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+else:
+    print(response.text)
